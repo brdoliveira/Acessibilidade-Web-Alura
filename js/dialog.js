3328131a637c8a284ac3d1a3af825a769ef85276
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-06-09 23:49:50 -0300

    FEAT(dialog): Adiiconando configuração do dialog

--- js/dialog.js
+++ js/dialog.js
@@ -3,19 +3,27 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
+var video = document.querySelector('video');
 
 btnAbreDialog.getElementsByClassName.display = 'block'
 
 
 // Quando abrir a dialog...
+// Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
-  document.querySelector('.dialogNewsletter-campo').focus();
+  document.querySelector('.dialogNewsletter-label').focus();
+  conteudoForaDialog.inert = true;
+  video.removeAttribute('controls');
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false  ;
+  btnAbreDialog.focus();
+  video.setAttribute('controls',true);
 }
 
 // Listeners
